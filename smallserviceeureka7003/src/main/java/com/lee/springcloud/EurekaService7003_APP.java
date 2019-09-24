831bb42a9e8cb2576ed2542c0866f86a125cc650
lee <781979436@qq.com>
2019-09-25 00:09:32 +0800

    eureka 集群

---
+++ smallserviceeureka7003/src/main/java/com/lee/springcloud/EurekaService7003_APP.java
@@ -0,0 +1,14 @@
+package com.lee.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //EurekaService 启动类 接受其他其他微服务的注册
+public class EurekaService7003_APP {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaService7003_APP.class,args);
+    }
+}
