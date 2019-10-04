20ff9cad25329779dd3e73a8a3cf2c8193fa4c26
lee <781979436@qq.com>
2019-10-04 16:13:51 +0800

    springcloud config 读取github配置文件

--- smallserviceconfigclient3355/src/main/java/com/lee/springcloud/ConfigClient_3355_StartSpringCloudApp.java
+++ smallserviceconfigclient3355/src/main/java/com/lee/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -1,14 +1,13 @@
 package com.lee.springcloud;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
-public class Config_3344_StartSpringCloudApp {
+public class ConfigClient_3355_StartSpringCloudApp
+{
     public static void main(String[] args)
     {
-        SpringApplication.run(Config_3344_StartSpringCloudApp.class,args);
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class,args);
     }
 }
+
